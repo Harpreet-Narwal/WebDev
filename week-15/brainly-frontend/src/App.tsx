da5c99bd49b338d05e8b9c44e63996bf8ad2e5d9
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2025-09-06 00:15:27 +0530

    Week-15 Part-2 done

--- week-15/brainly-frontend/src/App.tsx
+++ week-15/brainly-frontend/src/App.tsx
@@ -7,14 +7,15 @@ import { PlusIcon } from './icons/PlusIcon'
 import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button startIcon={<PlusIcon size='lg'/>} variant='primary' text={'add content'} size='sm'/>
-      <Button variant='secondary' text='Add content' size='md'/>
-      <Button startIcon={<ShareIcon size='lg'/>} variant='secondary' text={'share'} size='lg' />
-    
+        <div className='flex items-cente' >
+          <Button startIcon={<PlusIcon />} variant='primary' text={'add content'} size='md'/>
+          <Button variant='secondary' text='Add content' size='md'/>
+          <Button startIcon={<ShareIcon />} variant='secondary' text={'share'} size='md' />
+        </div>
+
     </>
   )
 }
--- week-15/brainly-frontend/src/component/ui/Button.tsx
+++ week-15/brainly-frontend/src/component/ui/Button.tsx
@@ -1,3 +1,5 @@
+import { cloneElement } from "react";
+
 export interface ButtonProps{
     variant: "primary" | "secondary";
     size: "sm" | "md" | "lg";
@@ -13,20 +15,32 @@ const variantStyles = {
 }
 
 const sizeStyle = {
-    "sm" : "py-2 px-2",
-    "md" : "py-4 px-4",
-    "lg" : "py-6 px-6"
+    "sm" : "py-2 px-4 text-sm" ,
+    "md" : "py-2 px-4 text-base",
+    "lg" : "py-2 px-8 text-xl"
 }
 
-const defaultStyles = "rounded-md flex"
+const defaultStyles = "rounded-md m-1"
 
-export const Button = (props:ButtonProps) =>{
-    
-    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyle[props.size]}`} >
-        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}
+
+
+ export const Button = (props:ButtonProps) =>{
+
+    const sizedStartIcon = props.startIcon && cloneElement(props.startIcon, {size: props.size});
+    const sizedEndIcon = props.endIcon && cloneElement(props.endIcon, {size: props.size})
+
+
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyle[props.size]} hover: cursor-pointer`} >
+            <div className="flex items-center">
+                {props.startIcon ? <div className="pr-1">{sizedStartIcon}</div> : null}
+                <div className="pl-2 pr-2">
+                    {props.text}
+                </div>
+                {sizedEndIcon}
+            </div>
     </button>
 
-}
+ }
 
 
 <Button variant="primary" size="md" onClick={() => {}} text={"abcd"}/>
