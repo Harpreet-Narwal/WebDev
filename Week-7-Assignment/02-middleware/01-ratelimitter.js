78e7942dff2ef125d426447394d8a729f5133495
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2026-03-11 22:19:33 +0530

    Week-7 Part 2 Ass Done

--- Week-7-Assignment/02-middleware/01-ratelimitter.js
+++ Week-7-Assignment/02-middleware/01-ratelimitter.js
@@ -16,6 +16,26 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) =>{
+  const userId = req.headers['user-id'];
+
+  if(!userId){
+    return res.status(400).json({msg: "user-id header is requireed"})
+  }
+
+  if(numberOfRequestsForUser[userId] === undefined){
+    numberOfRequestsForUser[userId] = 1;
+  }else{
+    numberOfRequestsForUser[userId]++;
+  }
+
+  if(numberOfRequestsForUser[userId] > 5){
+    return res.status(404).json({msg: 'Too many requests'});
+  }
+  next();
+})
+
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
