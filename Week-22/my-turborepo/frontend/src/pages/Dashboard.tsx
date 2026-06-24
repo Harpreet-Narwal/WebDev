43e4466a10696f585fe608e70ebe6cb8125b4468
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2026-06-25 00:05:15 +0530

    Week-23 Done

--- Week-22/my-turborepo/frontend/src/pages/Dashboard.tsx
+++ Week-22/my-turborepo/frontend/src/pages/Dashboard.tsx
@@ -1,38 +1,53 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { BACKEND_URL } from "@/config";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 
 
-async function createAvatar({name, image} : {name: string, image: string}){
+async function createAvatar({name, url} : {name: string, url: string}){
     const response = await axios.post(`${BACKEND_URL}/api/v1/avatar`, {
-        name, image
+        name, image: url
     })
 
     return response.data;
+}
+
+async function getAvatars(){
+    const response = await axios.get(`${BACKEND_URL}/api/v1/avatars`)
+
+    return response.data.avatars;
+
 }
 
 export function Dashboard(){
     const [name, setName] = useState("");
     const [avatarUrl, setAvatarUrl] = useState("");
     const [avatars, setAvatars] = useState([]);
+    const queryClient =  useQueryClient();
 
     const mutation = useMutation({
         mutationFn: createAvatar,
         onSuccess: () =>{
-
+            queryClient.invalidateQueries({queryKey: ['avatars'] })
         }
     })
 
+
+/* 
     useEffect(() =>{
         axios.get(`${BACKEND_URL}/api/v1/avatars`)
             .then(response =>{
                 setAvatars(response.data.avatars)
             })
-    }, avatars)
+    }, [avatars]) */
+
+    const query = useQuery({
+        queryFn: getAvatars,
+        queryKey: ['avatars']
+    })
 
     
     return <div>
@@ -40,17 +55,21 @@ export function Dashboard(){
         <div className="w-md border rounded-md p-4 flex flex-col items-center">
             <Input placeholder="name" className="m-2" onChange={(e) => setName(e.target.value)}></Input>
             <Input placeholder="url"  className="m-2" onChange={(e) => setAvatarUrl(e.target.value)}></Input>
-            <Button variant={"outline"} className="m-2 w-full " onClick={() =>{
-                mutation.mutate({
+            <Button variant={"outline"} className="m-2 w-full " onClick={async () =>{
+                await mutation.mutateAsync({
                     name: name,
                     url: avatarUrl
                 })
+                
+
             }}>Create Avatar</Button>
         </div> 
 
         <div>
             <b>Avatars</b>
-            {JSON.stringify(avatars)}
+            {query.data.map((avatar: any) => <div> 
+                {avatar.name}
+            </div>)}
         </div>
     </div>
 }
